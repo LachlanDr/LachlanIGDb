126ae7bf6a4e882337d8670b499cd4283397ff3c
LachlanDr <100386428+LachlanDr@users.noreply.github.com>
2024-11-21 13:01:16 +1100

    Started.
    Step 4

---
+++ .database/queries.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE Guesses(id INTEGER PRIMARY KEY AUTOINCREMENT,
+--                     user_id INTEGER,
+--                     date DATE NOT NULL,
+--                     score INTEGER,
+--                     game TEXT NOT NULL);
+
+-- CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT,
+--                     username TEXT UNIQUE NOT NULL,
+--                     password NOT NULL);
