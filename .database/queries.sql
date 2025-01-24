24c3c2b5e4bc1725302ec7a59c41c4c118ab6ed7
LachlanDr <lachlan.dreyer@education.nsw.gov.au>
2025-01-24 19:19:30 +1100

    Updated.

--- .database/queries.sql
+++
@@ -1,9 +0,0 @@
--- CREATE TABLE Guesses(id INTEGER PRIMARY KEY AUTOINCREMENT,
---                     user_id INTEGER,
---                     date DATE NOT NULL,
---                     score INTEGER,
---                     game TEXT NOT NULL);
-
--- CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT,
---                     username TEXT UNIQUE NOT NULL,
---                     password NOT NULL);
